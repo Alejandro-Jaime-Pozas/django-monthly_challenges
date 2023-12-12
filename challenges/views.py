0940f6c802c91ea6bbdb9c0006cb0871990f63fe
Alex Jaime <ajaimepozas@gmail.com>
2023-12-12 16:46:20 -0600

    some

--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat in jan")
+# def january(request):
+#     return HttpResponse("Eat no meat in jan")
 
 
-def february(request):
-    return HttpResponse("Wash the dishes in feb")
+# def february(request):
+#     return HttpResponse("Wash the dishes in feb")
 
 
-def monthly_challenge(request):
-    return HttpResponse()
+def monthly_challenge(request, month): # second parameter should be identical to urls path string bw <> brackets
+    challenge_text = None 
+    if month == "january":
+        challenge_text = "Eat no meat in jan"
+    elif month == "february":
+        challenge_text = "Was the dishes in feb"
+    elif month == "march":
+        challenge_text = "Learn django in mar"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
